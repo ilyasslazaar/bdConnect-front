a423c8acb8e6e20864d3804290fb72e8aa37800d
ilyasslazaar <52575595+ilyasslazaar@users.noreply.github.com>
2019-09-06 15:41:25 +0100

    Update defaultValues.js

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -11,7 +11,7 @@ export const localeOptions = [
   { id: "fr", name: "French"},
 ];
 
-export const servicePath = "http://localhost:8080";
+export const servicePath = "http://95.216.68.252:3789";
 export const apiUrl = servicePath + "api/";
 export const CONNECTORS_ENDPOINT = servicePath + "/api/connectors";
 export const ALL_USERS_ENDPOINT = servicePath + "/api/users";
